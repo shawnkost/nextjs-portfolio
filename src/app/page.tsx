b6311bfe6ab2c2d76548c208002a87855e1caa5b
Shawn Kost <shawnmkost@gmail.com>
2023-11-28 19:28:20 -0800

    Create AboutMe section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import AboutMe from '@/app/ui/About/AboutMe';
 import { NavBar } from '@/app/ui/Navbar/Navbar';
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <main>
+        <div className='home-gradient bg-cover bg-center bg-no-repeat'>
+          <section>
+            <AboutMe />
+          </section>
+        </div>
+      </main>
     </>
   );
 }
